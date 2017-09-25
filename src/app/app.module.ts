88756a8d0ea42173a5c85b2f4cf0cc46a734793f
Mateo Nuñez <mateo@web.com.py>
2017-09-25 01:20:42 -0400

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdToolbarModule } from '@angular/material'; 
+import { MdToolbarModule, MdButtonModule, MdCheckboxModule  } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule,
+    MdToolbarModule, 
+    MdButtonModule, 
+    MdCheckboxModule,
+    
     FlexLayoutModule
   ],
   providers: [],
